302df76cecb76449b9bb3b7cdc22bda7f96451ac
sujalchavdaa <sujalchavda500@gmail.com>
2025-07-03 19:43:51 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -30,10 +30,10 @@ def duration(filename):
     return float(result.stdout)
 
 def get_mps_and_keys(api_url):
-    response = requests.get(api_url)
+    response = requests.get(api_url) 
     response_json = response.json()
-    mpd = response_json.get('MPD')
-    keys = response_json.get('KEYS')
+    mpd = response_json.get('mpd_url')
+    keys = response_json.get('keys')
     return mpd, keys
    
 def exec(cmd):
