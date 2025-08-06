30e8cc6d729e6b185eac9aef356f24c64c9bbfae
sujalchavdaa <sujalchavda500@gmail.com>
2025-08-06 22:02:44 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -9,7 +9,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "8118667253"))
 CREDIT = "✿𝐒𝐔𝐉𝐀𝐋✿"
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8118667253').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7114926879').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
@@ -21,3 +21,4 @@ COLLECTION_NAME = environ.get("COLLECTION_NAME", "Telegrambot")
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
